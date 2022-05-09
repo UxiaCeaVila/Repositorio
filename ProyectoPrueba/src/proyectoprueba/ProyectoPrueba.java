58c2f32a18f8b305118fd82e6128ee562766bfcb
Mario González Besada <mariogonzalezgoian@gmail.com>
2022-05-09 09:27:42 +0200

    Commit

--- ProyectoPrueba/src/proyectoprueba/ProyectoPrueba.java
+++ ProyectoPrueba/src/proyectoprueba/ProyectoPrueba.java
@@ -17,6 +17,7 @@ public class ProyectoPrueba {
     public static void main(String[] args) {
         // TODO code application logic here
         //Nuevo
+        // Commit Mario González Besada
     }
     
 }
